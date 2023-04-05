5389a4d6a925c6e6b208e809da8bada8fa1663e1
Petr <petr.cechura@tutanota.com>
2023-04-05 16:41:53 +0200

    MD reworked (TODO)

--- ManchesterDecoder.vhd
+++ ManchesterDecoder.vhd
@@ -17,8 +17,7 @@ entity ManchesterDecoder is
         reset : in std_logic;
         in_positive, in_negative : in std_logic;
         data_out : out std_logic;
-        data_change : out std_logic;
-        sync_type : out std_logic;
+        RX_done : out std_logic;
         state_out : out std_logic_vector(2 downto 0)
     );
 end entity;
@@ -26,175 +25,209 @@ end entity;
 
 
 architecture rtl of ManchesterDecoder is
-    --synchronize detector--
+    -- SYNCHRONIZE DETECTOR --
     type t_state is (s_default, --default state, terminal is waiting for signal
-                     s_logic_one1, -- pos input is detected; state is held until 3/2 period or when input changes its value (invalid sync)
-                     s_logic_one2, -- neg input is detected after previous pos stood still for 3/2 period; when this value remains unchanged for 3/2 period, sync was succesful
-                     s_logic_two1, -- same but viceversa
-                     s_logic_two2, -- same but viceversa
+                     s_logic_one1, --
+                     s_logic_one2,
+                     s_logic_one3, 
+                     s_logic_two1, 
+                     s_logic_two2,
+                     s_logic_two3, 
                      s_synchronize); -- stands for 17 manchester bit periods (measured just by time and known frequency); then goes to default
+    signal state_q, state_d : t_state;
 
-    signal timer_sig : unsigned(5 downto 0);
-    signal timer_enable : std_logic;
-    signal timer_max : std_logic;
-    signal state : t_state := s_default;
-    --synchronize word count; how many bits are after sync
-    signal word_count : unsigned(4 downto 0) := (others => '0');
-    constant word_size : integer := 17; 
-    
-    --manchester decoder--
-    signal cnt_sig : unsigned(4 downto 0);
-    signal data : std_logic := '0';
-    signal clk_en : std_logic := '0';
-    
+    --timer for sync sample
+    signal sync_timer_en : std_logic;
+    signal sync_timer_d, sync_timer_q : unsigned(4 downto 0);
+    signal sync_timer_max : std_logic;
+
+    -- MANCHESTER DECODER --
+    --timer for data sample
+    signal manchester_timer_en : std_logic;
+    signal manchester_timer_d, manchester_timer_q : unsigned(4 downto 0);
+    signal manchester_timer_sample : std_logic;
+
+    --counter for data count
+    signal data_counter_en : std_logic;
+    signal data_counter_d, data_counter_q : unsigned(3 downto 0); -- counts to 16 (16-bit word)
+    signal data_counter_max : std_logic;
+    --data register
+    signal decoded_data : unsigned(3 downto 0);
+
+
+
+
     
 begin
 
-    ---SYNCHRONIZE DETECTOR---
-    process (clk, reset)
+    --*** SYNCHRONIZE DETECTOR ***--
+
+    -- MAIN STATE MACHINE
+    --seq part
+    process (clk)
     begin
         if reset='1' then
-            state <= s_default;
+            state_q <= s_default;
         elsif rising_edge(clk) then
-            case state is 
-                when s_default =>
-                    if in_positive='1' then
-                        state <= s_logic_one1;
-                    elsif in_negative='1' then
-                        state <= s_logic_two1;
-                    else
-                        state <= s_default;
-                    end if;
-                when s_logic_one1 =>
-                    if timer_max='1' then
-                        state <= s_logic_one2;
-                    elsif in_positive='1' then
-                        state <= s_logic_one1;
-                    else
-                        state <= s_default;
-                    end if;
-                when s_logic_one2 =>
-                    if timer_max='1' then
-                        state <= s_synchronize;
-                        sync_type <= '0'; --com word
-                    elsif in_negative='1' then
-                        state <= s_logic_one2;
-                    else
-                        state <= s_default;
-                    end if;
-                when s_logic_two1 =>
-                    if timer_max='1' then
-                        state <= s_logic_two2;
-                    elsif in_negative = '1' then
-                        state <= s_logic_two1;
-                    else
-                        state <= s_default;
-                    end if;
-                when s_logic_two2 =>
-                    if timer_max='1' then
-                        state <= s_synchronize;
-                    elsif in_positive='1' then
-                        state <= s_logic_two2;
-                    else 
-                        state <= s_default;
-                    end if;
-                when s_synchronize =>
-                    if word_count = word_size then
-                        state <= s_default;
-                        sync_type <= '0';
-                    else
-                        state <= s_synchronize;
-                    end if;
-            end case;
+            state_q <= state_d;
         end if;
     end process;
 
-    process (state, in_positive, in_negative)
+    --comb part
+    process (state_q)
     begin
-        timer_enable <= '0' when state=s_default
-                        else '1';
+        case state_q is 
+            when s_default =>
+                sync_timer_en <= '0';
+                manchester_timer_en <= '0';
 
-        if state=s_synchronize and in_positive /= in_negative then
-            data <= '1' when in_positive='1'
-                    else '0';
-            clk_en <= '1';
-        else
-            data <= 'Z';
-            clk_en <= '0';
-        end if;
+                if in_positive= '1' and in_negative='0' then
+                    state_d <= s_logic_one1;
+                elsif in_negative = '1' and in_positive = '0' then
+                    state_d <= s_logic_two1;
+                else
+                    state_d <= s_default;
+                end if;
+            when s_logic_one1 =>
+                sync_timer_en <= '1';
+                manchester_timer_en <= '0';
 
+                if sync_timer_max='1' then
+                    state_d <= s_logic_one2;
+                elsif in_positive='1' and in_negative='0' then
+                    state_d <= s_logic_one1;
+                else
+                    state_d <= s_default;
+                end if;
+
+            when s_logic_one2 =>
+                sync_timer_en <= '1';
+                manchester_timer_en <= '0';
+
+                if sync_timer_max='1' then
+                    state_d <= s_logic_one3;
+                else
+                    state_d <= s_logic_one2;
+                end if;
+
+            when s_logic_one3 =>
+                sync_timer_en <= '1';
+                manchester_timer_en <= '0';
+
+                if sync_timer_max='1' then
+                    state_d <= s_synchronize;
+                elsif in_positive='0' and in_negative='1' then
+                    state_d <= s_logic_one3;
+                else
+                    state_d <= s_default;
+                end if;
+
+            when s_logic_two1 =>
+                sync_timer_en <= '1';
+                manchester_timer_en <= '0';
+
+                if sync_timer_max='1' then
+                    state_d <= s_logic_two2;
+                elsif in_positive='0' and in_negative='1' then
+                    state_d <= s_logic_two1;
+                else
+                    state_d <= s_default;
+                end if;
+            when s_logic_two2 =>
+                sync_timer_en <= '1';
+                manchester_timer_en <= '0';
+
+                if sync_timer_max='1' then
+                    state_d <= s_logic_two3;
+                else
+                    state_d <= s_logic_two2;
+                end if;
+            when s_logic_two3 =>
+                sync_timer_en <= '1';
+                manchester_timer_en <= '0';
+
+                if sync_timer_max='1' then
+                    state_d <= s_synchronize;
+                elsif in_positive='1' and in_negative='0' then
+                    state_d <= s_logic_two3;
+                else
+                    state_d <= s_default;
+                end if;
+
+            when s_synchronize =>
+                sync_timer_en <= '0';
+                manchester_timer_en <= '1';
+
+
+        end case;
     end process;
 
-    --timer
+    -- SYNC TIMER SAMPLE
+    --seq part
     process (clk)
     begin
-        if reset = '1' then
-            timer_sig <= (others => '0');
+        if reset='1' then
+            sync_timer_q <= (others => '0'); 
         elsif rising_edge(clk) then
-            if timer_enable='1' and timer_sig=46 then
-                timer_sig <= (others => '0');
-            elsif timer_enable='1' then
-                timer_sig <= timer_sig+1;
-            else
-                timer_sig <= (others => '0');
-            end if;
+            sync_timer_q <= sync_timer_d;
         end if;
     end process;
 
-    timer_max <= '1' when timer_sig=46 else '0';
-
-    --word count
-    process (clk, timer_max)
+    -- comb part
+    process (sync_timer_q)
     begin
-        if reset = '1' then
-            word_count <= (others => '0');
-        elsif rising_edge(clk) then
-            if timer_max='1' and state=s_synchronize then
-                word_count <= word_count+1;
-            elsif timer_max='1' then
-                word_count <= (others => '0');
-            else
-                word_count <= word_count;
-            end if;
+        if sync_timer_en='1' then
+            sync_timer_d <= sync_timer_q+1;
+        else
+            sync_timer_d <= (others => '0');
+        end if; 
+
+        if sync_timer_q = (others => '1') then
+            sync_timer_max <= '1';
         end if;
+
     end process;
 
+    --- *** MANCHESTER SAMPLE *** ---
 
-    -- state machine show
-    state_out <=        "000" when state=s_default
-                else    "001" when state=s_logic_one1
-                else    "010" when state=s_logic_one2
-                else    "011" when state=s_logic_two1
-                else    "100" when state=s_logic_two2
-                else    "111" when state=s_synchronize;
-
-    ---MANCHESTER DECODER---
+    -- MANCHESTER TIMER SAMPLE
+    --seq part
     process (clk)
     begin
-        if reset = '1' then
-            cnt_sig <= (others => '0');
+        if reset='1' then
+            manchester_timer_q <= (others => '0'); 
         elsif rising_edge(clk) then
-            if clk_en='1' then
-                cnt_sig <= cnt_sig + 1;
-            else
-                cnt_sig <= cnt_sig;
-            end if;
+            manchester_timer_q <= manchester_timer_d;
         end if;
     end process;
 
-    process (cnt_sig, reset)
+    --comb part
+    process (manchester_timer_q)
     begin
-        if reset = '1' then
-            data_out <= '0';
-            data_change <= '0';
-        elsif cnt_sig = 24 then --get value in 3/4 period
-            data_out <= data;
-            data_change <= '1';
+        if manchester_timer_en='1' then
+            manchester_timer_d <= manchester_timer_q + 1;
         else
-            data_out <= data_out;
-            data_change <= '0';
+            manchester_timer_d <= (others => '0'); 
+        end if;
+
+        if manchester_timer_q=24 then
+            manchester_timer_sample <= '1';
+        else
+            manchester_timer_sample <= '0';
+        end if;
+
+    end process;
+
+    -- DATA REGISTER
+    process (clk)
+    begin
+        if rising_edge(clk) then
+            
         end if;
     end process;
-    
+
+
+
+
 
 end architecture;
